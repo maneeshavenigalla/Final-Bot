bcdd939c480fdc66c3264959cf90e866519c4305
Maneesha Venigalla <maneesha@Maneeshas-MacBook-Pro.local>
2019-07-05 12:44:12 +0530

    Implement hotel and room booking with image integration

--- app.js
+++ app.js
@@ -1,144 +1,195 @@
-// This loads the environment variables from the .env file
-require('dotenv-extended').load();
+require("dotenv-extended").load();
 
-const builder = require('botbuilder');
-const restify = require('restify');
-const Store = require('./store');
-const spellService = require('./spell-service');
+const builder = require("botbuilder");
+const restify = require("restify");
+const Store = require("./store");
+const spellService = require("./spell-service");
 
-// Setup Restify Server
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`${server.name} listening to ${server.url}`);
+  console.log(`${server.name} listening to ${server.url}`);
 });
-// Create connector and listen for messages
+
 const connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-server.post('/api/messages', connector.listen());
+server.post("/api/messages", connector.listen());
 
-
-// Default store: volatile in-memory store - Only for prototyping!
 var inMemoryStorage = new builder.MemoryBotStorage();
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
-}).set('storage', inMemoryStorage); // Register in memory storage
+var bot = new builder.UniversalBot(connector, function(session) {
+  session.send(
+    "Hello!! I`m the Constance Bot! Your friendly Bot! How can I assist you ?",
+    session.message.text
+  );
+}).set("storage", inMemoryStorage);
 
-
-// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
-// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
 const recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
-bot.dialog('SearchHotels', [
+bot
+  .dialog("SearchHotels", [
     (session, args, next) => {
-        session.send(`Welcome to the Hotels finder! We are analyzing your message: 'session.message.text'`);
-        // try extracting entities
-        const cityEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city');
-        const airportEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'AirportCode');
-        if (cityEntity) {
-            // city entity detected, continue to next step
-            session.dialogData.searchType = 'city';
-            next({ response: cityEntity.entity });
-        } else if (airportEntity) {
-            // airport entity detected, continue to next step
-            session.dialogData.searchType = 'airport';
-            next({ response: airportEntity.entity });
-        } else {
-            // no entities detected, ask user for a destination
-            builder.Prompts.text(session, 'Please enter your destination');
-        }
+      session.send(
+        `Welcome to the Constance! We are analyzing your request: ${
+          session.message.text
+        }. Please give us a minute!`
+      );
+      const cityEntity = builder.EntityRecognizer.findEntity(
+        args.intent.entities,
+        "builtin.geography.city"
+      );
+      const airportEntity = builder.EntityRecognizer.findEntity(
+        args.intent.entities,
+        "AirportCode"
+      );
+      if (cityEntity) {
+        session.dialogData.searchType = "city";
+        next({ response: cityEntity.entity });
+      } else if (airportEntity) {
+        session.dialogData.searchType = "airport";
+        next({ response: airportEntity.entity });
+      } else {
+        builder.Prompts.text(session, "Please enter your destination");
+      }
     },
     (session, results) => {
-        const destination = results.response;
-        let message = 'Looking for hotels';
-        if (session.dialogData.searchType === 'airport') {
-            message += ' near %s airport...';
-        } else {
-            message += ' in %s...';
-        }
-        session.send(message, destination);
-        // Async search
-        Store
-            .searchHotels(destination)
-            .then(hotels => {
-                // args
-                session.send(`I found ${hotels.length} hotels:`);
-                let message = new builder.Message()
-                    .attachmentLayout(builder.AttachmentLayout.carousel)
-                    .attachments(hotels.map(hotelAsAttachment));
-                session.send(message);
-                // End
-                session.endDialog();
-            });
+      const destination = results.response;
+      let message = "Looking for hotels";
+      if (session.dialogData.searchType === "airport") {
+        message += " near %s airport...";
+      } else {
+        message += " in %s...";
+      }
+      session.send(message, destination);
+      Store.searchHotels(destination).then(hotels => {
+        session.send(`I found ${hotels.length} hotels:`);
+        let message = new builder.Message()
+          .attachmentLayout(builder.AttachmentLayout.carousel)
+          .attachments(hotels.map(hotelAsAttachment));
+        session.send(message);
+        session.endDialog();
+      });
     }
-]).triggerAction({
-    matches: 'SearchHotels',
-    onInterrupted:  session => {
-        session.send('Please provide a destination');
+  ])
+  .triggerAction({
+    matches: "SearchHotels",
+    onInterrupted: session => {
+      session.send("Please provide a destination");
     }
-});
+  });
 
-bot.dialog('ShowHotelsReviews', (session, args) => {
-    // retrieve hotel name from matched entities
-    const hotelEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Hotel');
+bot
+  .dialog("Rooms", [
+    (session, args, next) => {
+      session.send(`We are fetching the rooms as per your request.!`);
+      next({ response: args });
+    },
+    (session, results) => {
+      const typeOfRoom = results.response;
+      let message = "Looking for available rooms";
+      session.send(message, typeOfRoom);
+      Store.searchRooms(typeOfRoom).then(rooms => {
+        session.send(`I found ${rooms} rooms:`);
+        let availableRooms = new builder.Message().attachmentLayout(
+          builder.AttachmentLayout.carousel
+        );
+        //   .attachments(rooms.map(roomAsAttachment));
+        session.send(availableRooms);
+        session.endDialog();
+      });
+    }
+  ])
+  .triggerAction({
+    matches: "Rooms",
+    onInterrupted: session => {
+      session.send("Please provide a valid choice");
+    }
+  });
+
+bot
+  .dialog("ShowHotelsReviews", (session, args) => {
+    const hotelEntity = builder.EntityRecognizer.findEntity(
+      args.intent.entities,
+      "Hotel"
+    );
     if (hotelEntity) {
-        session.send(`Looking for reviews of '${hotelEntity.entity}'...`);
-        Store
-            .searchHotelReviews(hotelEntity.entity)
-            .then(reviews => {
-                let message = new builder.Message()
-                    .attachmentLayout(builder.AttachmentLayout.carousel)
-                    .attachments(reviews.map(reviewAsAttachment));
-                session.endDialog(message);
-            });
+      session.send(`Looking for reviews of '${hotelEntity.entity}'...`);
+      Store.searchHotelReviews(hotelEntity.entity).then(reviews => {
+        let message = new builder.Message()
+          .attachmentLayout(builder.AttachmentLayout.carousel)
+          .attachments(reviews.map(reviewAsAttachment));
+        session.endDialog(message);
+      });
     }
-}).triggerAction({
-    matches: 'ShowHotelsReviews'
-});
+  })
+  .triggerAction({
+    matches: "ShowHotelsReviews"
+  });
 
-bot.dialog('Help', session => {
-    session.endDialog(`Hi! Try asking me things like 'search hotels in Seattle', 'search hotels near LAX airport' or 'show me the reviews of The Bot Resort'`);
-}).triggerAction({
-    matches: 'Help'
-});
+bot
+  .dialog("Help", session => {
+    session.endDialog(
+      `Try asking me things like 'search hotels in Mauritius' or 'show me the reviews of Constance Prince Maurice'`
+    );
+  })
+  .triggerAction({
+    matches: "Help"
+  });
 
-// Spell Check
-if (process.env.IS_SPELL_CORRECTION_ENABLED === 'true') {
-    bot.use({
-        botbuilder: (session, next) => {
-            spellService
-                .getCorrectedText(session.message.text)
-                .then(text => {
-                    session.message.text = text;
-                    next();
-                })
-                .catch(error => {
-                    console.error(error);
-                    next();
-                });
-        }
-    });
+if (process.env.IS_SPELL_CORRECTION_ENABLED === "true") {
+  bot.use({
+    botbuilder: (session, next) => {
+      spellService
+        .getCorrectedText(session.message.text)
+        .then(text => {
+          session.message.text = text;
+          next();
+        })
+        .catch(error => {
+          console.error(error);
+          next();
+        });
+    }
+  });
 }
 
-// Helpers
 const hotelAsAttachment = hotel => {
-    return new builder.HeroCard()
-        .title(hotel.name)
-        .subtitle('%d stars. %d reviews. From $%d per night.', hotel.rating, hotel.numberOfReviews, hotel.priceStarting)
-        .images([new builder.CardImage().url(hotel.image)])
-        .buttons([
-            new builder.CardAction()
-                .title('More details')
-                .type('openUrl')
-                .value('https://www.bing.com/search?q=hotels+in+' + encodeURIComponent(hotel.location))
-        ]);
-}
+  return new builder.HeroCard()
+    .title(hotel.name)
+    .subtitle(
+      "%d stars. %d reviews. From $%d per night.",
+      hotel.rating,
+      hotel.numberOfReviews,
+      hotel.priceStarting
+    )
+    .images([new builder.CardImage().url(hotel.image)])
+    .buttons([
+      new builder.CardAction()
+        .title("More details")
+        .type("openUrl")
+        .value("https://www.constancehotels.com/en/")
+    ]);
+};
+
+const roomAsAttachment = room => {
+  return (
+    new builder.HeroCard()
+      .title(room.name)
+      .subtitle("%d stars. %d reviews. From $%d per night.", room.name)
+      // .images([new builder.CardImage().url(hotel.image)])
+      .buttons([
+        new builder.CardAction()
+          .title("More details")
+          .type("openUrl")
+          .value("https://www.constancehotels.com/en/")
+      ])
+  );
+};
 
 const reviewAsAttachment = review => {
-    return new builder.ThumbnailCard()
-        .title(review.title)
-        .text(review.text)
-        .images([new builder.CardImage().url(review.image)]);
-}
+  return new builder.ThumbnailCard()
+    .title(review.title)
+    .text(review.text)
+    .images([new builder.CardImage().url(review.image)]);
+};
